9f8ab09fa8476f18052285e73758123ee29d7d27
I3Tank <64980879+I3Tank@users.noreply.github.com>
2022-04-19 13:20:34 +0200

    First Session Done -Task added

--- app/src/main/java/com/kevus/necessary/widgets/TaskWidgets.kt
+++ app/src/main/java/com/kevus/necessary/widgets/TaskWidgets.kt
@@ -1,12 +1,13 @@
 package com.kevus.necessary.widgets
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -15,15 +16,30 @@ import com.kevus.necessary.models.getTestTask
 import java.time.LocalTime
 
 @Composable
-fun TaskBox(Task: Task){
+fun TaskBox(Task: Task) {
     val endTime = calculateEndTime(Task)
 
-    Card(shape = RoundedCornerShape(corner = CornerSize(10.dp))) {
-         Column(modifier = Modifier.padding(5.dp)) {
-             Text(text = Task.TaskName)
-             Text(text = Task.TaskDescription)
-             Text(text = Task.TaskDate.toLocalTime().toString() + " - " + endTime.toString())
-         }
+    Card(
+        modifier = Modifier
+            .width(100.dp)
+            .height(Task.TaskDuration.dp),
+        shape = RoundedCornerShape(corner = CornerSize(10.dp)),
+    ) {
+        Column(
+            modifier = Modifier.padding(5.dp),
+            verticalArrangement = Arrangement.Center,
+        ) {
+            Text(
+                text = Task.TaskName,
+                modifier = Modifier.align(Alignment.CenterHorizontally),
+                style = MaterialTheme.typography.h6
+            )
+            Text(
+                text = Task.TaskDate.toLocalTime().toString() + " - " + endTime.toString(),
+                modifier = Modifier.align(Alignment.CenterHorizontally),
+                style = MaterialTheme.typography.overline
+            )
+        }
     }
 }
 
